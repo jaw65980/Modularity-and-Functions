dc5aac2e936ad94275f91b32e4c9499fed4647ea
Jaden White <jaw65980@email.vccs.edu>
2024-10-04 22:13:24 -0400

    Finished the program

--- Modularity and Functions.cpp
+++ Modularity and Functions.cpp
@@ -2,7 +2,41 @@
 #include <iostream>
 using namespace std;
 
+float Priceoutput(float oneyearResult, float twoyearResult)
+{
+	cout << endl << "Inflation rate changed by " << oneyearResult << " one year ago and " << twoyearResult << " two years ago." << endl;
+	return 0;
+
+}
+
+float priceChange(float currentPrice, float oneyearPrice, float twoyearPrice)
+{
+	float oneyearInflation = currentPrice - oneyearPrice;
+	oneyearInflation = oneyearInflation / oneyearPrice;
+
+	float twoyearInflation;
+	twoyearInflation = currentPrice - twoyearPrice;
+	twoyearInflation = twoyearInflation / twoyearPrice;
+	Priceoutput(oneyearInflation, twoyearInflation);
+	return 0;
+}
+
+float Priceinput()
+{
+	float currentPrice;
+	float oneyearPrice;
+	float twoyearPrice;
+	cout << "What is the current price of your chosen item? ";
+	cin >> currentPrice;
+	cout << endl << "What was the price of your item one year ago? ";
+	cin >> oneyearPrice;
+	cout << endl << "What was the price of your item two years ago? ";
+	cin >> twoyearPrice;
+	priceChange(currentPrice, oneyearPrice, twoyearPrice);
+	return 0;
+}
+
 int main()
 {
- 
+	Priceinput();
 }
